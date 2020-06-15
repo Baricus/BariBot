c9f01c3a9641bbe4c473b88379638b65610f93f7
Baricus <31926270+Baricus@users.noreply.github.com>
2020-06-15 16:20:09 -0500

    changing approach to a shared helper object

--- source/IRCCommandMap.cpp
+++
@@ -1,9 +0,0 @@
-/* IRCCommandMap.cpp - Miles Shamo
- *
- * This is the implementation file which
- * creates a map of strings to functions.
- *
- * Each string is an IRC command and each
- * function is given 
- *
- */
